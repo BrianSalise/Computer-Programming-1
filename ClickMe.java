6827460da57c92379a3e8a7477dc1bd8252a9581
Brian Salise <briansalise12@gmail.com>
2023-10-05 17:02:46 +0900

    main

--- ClickMe.java
+++ ClickMe.java
@@ -1,4 +1,18 @@
 class ClickMe {
-  public static void main(String args[]) {
-    System.out.println("Pull")
+  public static void main(String[] args) {
+    System.out.println("Pull");
+    System.out.println("");
+    String name = "MY NAME IS BRIAN";
+
+    for(int i = 0;i < name.length();i++){
+      System.out.println(name.charAt(i) + "");
+      try {
+        Thread.sleep(100);
+      }catch (Exception e) {
+        e.printStackTrace();
+      }
+
+    }
+
+  }
 }
